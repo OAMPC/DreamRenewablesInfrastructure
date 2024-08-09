082d01e9efb742f8184eaaa20aafc1d08e67fe93
popey2700 <alex.pope@madetech.com>
2024-08-09 19:34:05 +0100

    Add CMS terraform

---
+++ cms-terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "heroku_cms_app_name" {
+  description = "Name of the Heroku app"
+  default     = "dream-renewables-strapi"
+}
+
+variable "heroku_cms_db_name" {
+  description = "Name of the Heroku app db"
+  default     = "dream-renewables-strapi-postgres-db"
+}
