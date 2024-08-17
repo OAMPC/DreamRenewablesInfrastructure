cda1b7444c547491f0bdcabeb9e99cd355120e79
popey2700 <alexanderpope27@outlook.com>
2024-08-17 23:10:41 +0100

    Husky fix 2

--- cms-terraform/heroku-db.tf
+++ cms-terraform/heroku-db.tf
@@ -1,5 +1,5 @@
 resource "heroku_addon" "strapi_postgres_db" {
-  name=   var.heroku_cms_db_name
-  app_id =      heroku_app.dream_renewables_strapi.id
+  name   = var.heroku_cms_db_name
+  app_id = heroku_app.dream_renewables_strapi.id
   plan   = "heroku-postgresql:essential-1"
 }
