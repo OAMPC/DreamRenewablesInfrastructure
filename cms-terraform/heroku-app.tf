8c86dafb4328f2f9635ebeeda584058309cf2d7e
popey2700 <alexanderpope27@outlook.com>
2024-08-17 22:45:59 +0100

    move heroku resources
    I am going to be adding some more aws resources
    and as a result i do not want it getting muddled
    with heorku

---
+++ cms-terraform/heroku-app.tf
@@ -0,0 +1,4 @@
+resource "heroku_app" "dream_renewables_strapi" {
+  name   = var.heroku_cms_app_name
+  region = "eu"
+}
--- cms-terraform/main.tf
+++ cms-terraform/main.tf
@@ -6,14 +6,3 @@ terraform {
     }
   }
 }
-
-resource "heroku_app" "dream_renewables_strapi" {
-  name   = var.heroku_cms_app_name
-  region = "eu"
-}
-
-resource "heroku_addon" "strapi_postgres_db" {
-  name   = var.heroku_cms_db_name
-  app_id = heroku_app.dream_renewables_strapi.id
-  plan   = "heroku-postgresql:essential-1"
-}
